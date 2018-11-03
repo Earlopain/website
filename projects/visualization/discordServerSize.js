99409d3a5ab8d9b36b583b29d5109315bd32ca17
Earlopain <earlopain@c5h8no4na.net>
2018-11-03 16:24:39 +0100

    Serverside script to get server population

---
+++ projects/visualization/discordServerSize.js
@@ -0,0 +1,75 @@
+const request = require("request");
+const fs = require("fs");
+
+const servers = ["Z9Zc9cE", "eC72Vj9", "b2tDDVd"];
+const outputFolder = "./discordoutput";
+try {
+    fs.mkdirSync(outputFolder);
+} catch (error) {/*EEXIST*/ }
+
+const checkInterval = 20 //seconds
+
+async function main() {
+    for (let i = 0; i < servers.length; i++) {
+        if (!fs.existsSync(outputFolder + "/" + servers[i] + ".csv")) {
+            const serverName = await getServerName(servers[i]);
+            fs.appendFileSync(outputFolder + "/" + servers[i] + ".csv", "Date,Count," + serverName + "\n");
+        }
+    }
+    while (true) {
+        const timeStart = new Date().getMilliseconds();
+        for (let i = 0; i < servers.length; i++) {
+            const dateString = getTime();
+            let skip = false;
+            let serverSize;
+            try {
+                serverSize = await getServerSize(servers[i]);
+            } catch (error) {
+                fs.appendFileSync(outputFolder + "/" + servers[i] + ".csv", dateString + ",Network Error,\n");
+                skip = true;
+            }
+            if (!skip) {
+                if (!serverSize) {
+                    console.log("Invite expired");
+                    fs.appendFileSync(outputFolder + "/" + servers[i] + ".csv", dateString + ",Invite Expired,\n");
+                }
+                else
+                    fs.appendFileSync(outputFolder + "/" + servers[i] + ".csv", dateString + "," + serverSize + ",\n");
+            }
+        }
+        const runtime = new Date().getMilliseconds() - timeStart;
+        await sleep(checkInterval * 1000 - runtime);
+    }
+}
+
+main();
+
+async function getServerSize(id) {
+    const json = await getJSON("https://discordapp.com/api/v6/invite/" + id + "?with_counts=true");
+    return json.approximate_member_count;
+}
+async function getServerName(id) {
+    const json = await getJSON("https://discordapp.com/api/v6/invite/" + id + "?with_counts=true");
+    return json.guild.name;
+}
+
+function getJSON(url) {
+    return new Promise(resolve => {
+        request.get(url, (error, response, body) => {
+            resolve(JSON.parse(body));
+        });
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getTime() {
+    const date = new Date();
+    const hour = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+    const result = date.getUTCFullYear() + "-" + (date.getUTCMonth() + 1) + "-" + date.getUTCDate() + " " + (hour.toString().length === 1 ? "0" + hour : hour) + ":" + (minutes.toString().length === 1 ? "0" + minutes : minutes) + ":" + (seconds.toString().length === 1 ? "0" + seconds : seconds);
+    return result;
+}
