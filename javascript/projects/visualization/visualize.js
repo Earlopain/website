1157c5d8796cf4517804e7ed78cecf2aafa2e6da
Earlopain <earlopain@c5h8no4na.net>
2018-12-29 15:44:43 +0100

    Moved common methods to util file

--- javascript/projects/visualization/visualize.js
+++ javascript/projects/visualization/visualize.js
@@ -171,35 +171,7 @@ function changeGraph() {    //gets called if dropdown menu selected value change
     loadGraph(dropdownAll.value);
 }
 
-//simply returns content of a url on same-origin
-function getURL(url) {
-    return new Promise((resolve, reject) => {
-        let request = new XMLHttpRequest();
-        request.open("GET", url, true);
-        request.onload = () => {
-            if (request.status >= 200 && request.status < 400) {
-                resolve(request.responseText);
-            } else { reject(); }
-        };
-        request.onerror = () => { reject() };
-        request.send();
-    })
-}
 
-//simply returns content of a url on same-origin
-function postURL(url, data) {
-    return new Promise((resolve, reject) => {
-        let request = new XMLHttpRequest();
-        request.open("POST", url, true);
-        request.onload = () => {
-            if (request.status >= 200 && request.status < 400) {
-                resolve(request.responseText);
-            } else { reject(); }
-        };
-        request.onerror = () => { reject() };
-        request.send(JSON.stringify(data));
-    })
-}
 
 async function loadData(id) {
     try {
