2cc3d97a9d8db8d28a0db32d2514369b48b4f39d
Earlopain <earlopain@c5h8no4na.net>
2019-01-02 17:34:50 +0100

    Removed unused code

--- serverside/projects/e621history/fetchFavs.php
+++ serverside/projects/e621history/fetchFavs.php
@@ -18,43 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
     $favs = shell_exec("node ./getUserFavs.js " . $obj["username"]);
     http_response_code(200);
-    echo json_encode($favs);
+    echo $favs;
 
-}
-
-function getFavs($username)
-{
-    $result = [];
-    $c = curl_init();
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($c, CURLOPT_USERAGENT, "favhistory/earlopain");
-    $page = 1;
-    while (true) {
-        curl_setopt($c, CURLOPT_URL, "https://e621.net/post/index.json?tags=fav:$username&limit=320&page=$page");
-        $json =  json_decode(curl_exec($c), true);
-        foreach ($json as $index => $item) {
-            array_push($result, $item);
-        }
-        if (sizeof($json) !== 320) {
-            return $result;
-        }
-        $page++;
-    }
-}
-
-function addMissingFavsToDB($favs)
-{
-    $writeToFile = false;
-    $json  = json_decode(file_get_contents("./posts.json"), true);
-    foreach ($favs as $index => $post) {
-        if (!isset($json[$post["md5"]])) {
-            $writeToFile = true;
-            $json[$post["md5"]] = $post;
-        }
-    }
-    if ($writeToFile) {
-        file_put_contents("./posts.json", json_encode($json));
-    }
 }
 
 function isValidUsername($username)
