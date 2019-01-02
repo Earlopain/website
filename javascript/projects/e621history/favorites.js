724f29ccf27697e82df26d518dabbbb76aa1d194
Earlopain <earlopain@c5h8no4na.net>
2019-01-02 17:35:37 +0100

    Return {} if error

--- javascript/projects/e621history/favorites.js
+++ javascript/projects/e621history/favorites.js
@@ -1,9 +1,12 @@
-async function displayGraph(){
+async function displayGraph() {
     const favs = fetchFavorites();
 }
 
-async function fetchFavorites(){
+async function fetchFavorites() {
     const username = document.getElementById("name").value;
-    const favs = JSON.parse(await postURL("/serverside/projects/e621history/fetchFavs.php", {username: username}));
-    return favs;
+    const response = await postURL("/serverside/projects/e621history/fetchFavs.php", { username: username });
+    logResponse(response);
+    if (response.status !== 200)
+        return {};
+    return JSON.parse(response.responseText);
 }
