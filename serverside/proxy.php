b245de91cf620e539a135e9fce996fc5388e3626
Earlopain <earlopain@c5h8no4na.net>
2019-02-25 22:21:12 +0100

    Usable both in post and local include

--- serverside/proxy.php
+++ serverside/proxy.php
@@ -11,14 +11,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo "No url specified";
         return;
     }
+    http_response_code(200);
+    echo proxyGetUrl($obj["url"]);
+}
 
+function proxyGetUrl($url){
     $c = curl_init();
-    curl_setopt($c, CURLOPT_URL, $obj["url"]);
+    curl_setopt($c, CURLOPT_URL, $url);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0");
     curl_setopt($c, CURLOPT_FOLLOWLOCATION, TRUE);
     $response =  curl_exec($c);
-    http_response_code(200);
-    echo $response;
-
+    return $response;
 }
