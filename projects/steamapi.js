54bad091734f2f370b3e883272e168b3a19ec254
Earlopain <earlopain@c5h8no4na.net>
2019-03-02 23:58:20 +0100

    Simplified adding new calls

--- projects/steamapi.js
+++ projects/steamapi.js
@@ -2,8 +2,16 @@ const proxyURL = "/serverside/projects/steamgames/apiwrapper.php"
 const steamURL = "https://api.steampowered.com/";
 
 //TODO: automaticly convert vanity to id
+//Make class?
 
 async function getGames(steamid){
-    const response =  await postURL(proxyURL, {url: steamURL + "IPlayerService/GetOwnedGames/v1/?format=json&include_appinfo=1&include_played_free_games=1&appids_filter=&steamid=" + steamid})
-    return response;
+    return await getWrapper("IPlayerService/GetOwnedGames/v1/?format=json&include_appinfo=1&include_played_free_games=1&appids_filter=&steamid=" + steamid);
+}
+
+
+
+
+async function getWrapper(url){
+    const request = await postURL(proxyURL, {url: steamURL + url});
+    return request.response;
 }
