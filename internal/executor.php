efbe5fcb45c54bd8bb487fb25b13939b42f4e213
Earlopain <earlopain@c5h8no4na.net>
2019-10-31 19:48:25 +0100

    Code cleanup

--- internal/executor.php
+++ internal/executor.php
@@ -30,31 +30,32 @@ function getCommand()
         case 'plextagimages':
             return "node /media/plex/cronjobs/filetagger/plexTagNewImages.js";
         case 'plexfixdates':
-            return "echo 'Stopping Server' && sudo service plexmediaserver stop && echo 'Server Stopped' && sudo node /media/plex/software/plexFixDateAdded.js && echo 'Starting Server' && sudo service plexmediaserver start && echo 'Server Started'";
+            return wrapPlexStop("sudo node /media/plex/software/plexFixDateAdded.js");
         case 'plexfixnames':
-            return "echo 'Stopping Server' && sudo service plexmediaserver stop && sudo node /media/plex/software/plexFixFileNames.js && sudo service plexmediaserver start && echo 'Starting Server' && echo 'Done'";
+            return wrapPlexStop("sudo node /media/plex/software/plexFixFileNames.js");
         case 'apache2restart':
             return "sudo service apache2 restart";
         case 'deezerdl':
-            $myfile = fopen("/media/plex/software/deezerdl/downloadLinks.txt", "w") or die("Unable to open file!");
-            fwrite($myfile, implode("\n", explode("|" , $_POST["link"])));
-            fclose($myfile);
+            $filePath = "/media/plex/software/deezerdl/downloadLinks.txt";
+            file_put_contents($filePath, $_POST["link"]);
             return "cd /media/plex/software/deezerdl && ./SMLoader -q MP3_320 -p /media/plex/plexmedia/Music -d all";
         case 'e621dl':
             return "node /media/plex/software/e621downloader.js '" . $_POST["link"] . "'";
         case 'musicvideo':
-            $filePath = "/media/plex/software/tempfiles/youtubedl.txt";
-            $myfile = fopen($filePath, "w") or die("Unable to open file!");
-            fwrite($myfile, $_POST["link"]);
-            fclose($myfile);
-            return "youtube-dl --write-thumbnail --no-cache-dir --no-playlist --batch-file {$filePath} -o '/media/plex/plexmedia/musicvideos/%(title)s.%(ext)s' && echo 'Done'";
+            return youtubedl("/media/plex/plexmedia/musicvideos/%(title)s.%(ext)s");
         case 'shortmovie':
-            $filePath = "/media/plex/software/tempfiles/youtubedl.txt";
-            $myfile = fopen($filePath, "w") or die("Unable to open file!");
-            fwrite($myfile, $_POST["link"]);
-            fclose($myfile);
-            return "youtube-dl --write-thumbnail --no-cache-dir --no-playlist --batch-file {$filePath} -o '/media/plex/plexmedia/shortmovies/%(title)s.%(ext)s' && echo 'Done'";
+            return youtubedl("/media/plex/plexmedia/shortmovies/%(title)s.%(ext)s");
         default:
             return "echo test";
     }
+}
+
+function youtubedl($targetFormat){
+    $filePath = "/media/plex/software/tempfiles/youtubedl.txt";
+    file_put_contents($filePath, $_POST["link"]);
+    return "youtube-dl --write-thumbnail --no-cache-dir --no-playlist --batch-file {$filePath} -o '{$targetFormat}'";
+}
+
+function wrapPlexStop($command){
+    return "echo 'Stopping Server' && sudo service plexmediaserver stop && echo 'Server Stopped' && " . $command . " && echo 'Starting Server' && sudo service plexmediaserver start && echo 'Server Started'";
 }
