7d2d0191f1d18a2a00721ab8bc3092b0aaf4bac9
Earlopain <earlopain@c5h8no4na.net>
2019-10-31 19:26:57 +0100

    Remove youtube channel dl

--- internal/executor.php
+++ internal/executor.php
@@ -54,12 +54,6 @@ function getCommand()
             fwrite($myfile, $_POST["link"]);
             fclose($myfile);
             return "youtube-dl --write-thumbnail --no-cache-dir --no-playlist --batch-file {$filePath} -o '/media/plex/plexmedia/shortmovies/%(title)s.%(ext)s' && echo 'Done'";
-        case 'youtubechannel':
-            $filePath = "/media/plex/software/youtubechannel.txt";
-            if($_POST["link"] !== ""){
-                file_put_contents($filePath, $_POST["link"] . "\n", FILE_APPEND);
-            }
-            return "youtube-dl --write-thumbnail --ignore-errors --no-cache-dir --batch-file {$filePath} -o '/media/plex/plexmedia/youtubechannels/%(uploader)s/%(title)s.%(ext)s' && echo 'Done'";
         default:
             return "echo test";
     }
