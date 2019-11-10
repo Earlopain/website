7f63c3b3dd3b011bb416e2a64f52d367a2630d28
Earlopain <earlopain@c5h8no4na.net>
2019-11-10 20:16:39 +0100

    Added table sort functionality

--- internal/explorer/display.js
+++ internal/explorer/display.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     getFolderContent();
+    registerTableSort();
 });
 
 
---
+++ internal/explorer/tablesort.js
@@ -0,0 +1,56 @@
+let currentOrder = [];
+let sortType = ["none", "string", "string", "string", "string", "string", "size", "string", "string"];
+
+function registerTableSort() {
+    let counter = 0;
+    for (const header of document.getElementById("tableheader").children) {
+        header.addEventListener("click", () => sortColum(header.cellIndex));
+        currentOrder.push(1);
+        counter++;
+    }
+}
+
+function sortColum(index) {
+    if(sortType[index] === "none")
+        return;
+    let container = document.getElementById("filecontents");
+    let allEntries = container.children;
+
+    allEntries = [...allEntries].slice();
+    container.innerHTML = "";
+    const dotdot = allEntries.shift();
+    switch (sortType[index]) {
+        case "string":
+            allEntries = allEntries.sort(stringSort.bind(index));
+            break;
+        case "size":
+                allEntries = allEntries.sort(sizeSort.bind(index));
+
+        default:
+            break;
+    }
+
+    currentOrder[index] *= -1;
+    container.appendChild(dotdot);
+
+    for (const entry of allEntries) {
+        container.appendChild(entry);
+    }
+}
+
+function convertToBytes(input) {
+    const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+    const split = input.split(" ");
+    const index = units.indexOf(split[1]);
+    return split[0] *= Math.pow(1024, index);
+}
+
+function stringSort(a, b) {
+    return currentOrder[this] * ('' + a.children[this].innerText).localeCompare(b.children[this].innerText);
+}
+
+function sizeSort(a, b) {
+    a = convertToBytes(a.children[this].innerText);
+    b = convertToBytes(b.children[this].innerText);
+    return currentOrder[this] * (a - b);
+}
