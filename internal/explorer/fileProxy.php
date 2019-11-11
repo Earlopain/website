16be9ce6eb18ac7b968875824c45d060b9d1529b
Earlopain <earlopain@c5h8no4na.net>
2019-11-11 21:23:51 +0100

    Fix not being able to see files
    The first entry is now always the parent folder

--- internal/explorer/fileProxy.php
+++ internal/explorer/fileProxy.php
@@ -3,8 +3,7 @@
 require_once "getFolderInfo.php";
 
 $dir = new DirectoryInfo(base64_decode($_REQUEST["folder"]), [$_REQUEST["id"]]);
-$file = $dir->entries[0];
-
+$file = $dir->entries[1];
 header('Content-Type: ' . mime_content_type($file->absolutePath));
 if ($_SERVER["REQUEST_METHOD"] !== "HEAD") {
     readfile($file->absolutePath);
