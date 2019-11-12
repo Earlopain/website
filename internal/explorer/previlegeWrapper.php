4be0bbb9abdc62c03833f189d3de25813cb406d7
Earlopain <earlopain@c5h8no4na.net>
2019-11-12 19:12:34 +0100

    Preparation to support multiple actions for sudo script

--- internal/explorer/previlegeWrapper.php
+++ internal/explorer/previlegeWrapper.php
@@ -1,7 +1,22 @@
 <?php
-$user = prepareString($_REQUEST["user"]);
-$passwd = prepareString($_REQUEST["password"]);
-echo shell_exec("sudo php -f /media/plex/html/internal/explorer/validatePassword.php '{$user}' '{$passwd}'");
+if (!isset($_REQUEST{"action"})) {
+    die("Action missing");
+}
+switch ($_REQUEST["action"]) {
+    case "validatePassword":
+        $result = sudoExec($_REQUEST["action"], $_REQUEST["user"], $_REQUEST["password"]);
+        echo $result;
+        break;
+}
+
+function sudoExec($action, ...$args) {
+    $argString = "";
+    foreach ($args as $string) {
+        $argString .= "'" . prepareString($string) . "' ";
+    }
+    $action = prepareString($action);
+    return shell_exec("sudo php -f /media/plex/html/internal/explorer/sudoScript.php '{$action}' " . $argString);
+}
 
 function prepareString(string $string) {
     $split = explode("'", $string);
