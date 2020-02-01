75d71f0d211c61cb3fbd03de1de8e49dd529b6fd
Earlopain <earlopain@c5h8no4na.net>
2020-02-01 21:28:37 +0100

    Reuse connections

--- global/sql.php
+++ global/sql.php
@@ -2,12 +2,17 @@
 require_once "secretParser.php";
 
 class SqlConnection {
+    private static $connections = [];
+
     public static function get($dbname): PDO {
-        $servername = "localhost";
-        $username = "earlopain";
-        $password = getSecret("dbpass");
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
+        if (!isset(self::$connections[$dbname])) {
+            $servername = "localhost";
+            $username = "earlopain";
+            $password = getSecret("dbpass");
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$connections[$dbname] = $conn;
+        }
+        return self::$connections[$dbname];
     }
 }
