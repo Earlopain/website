22631c16ec1efb56294c57bac506cf98aa202996
Earlopain <earlopain@c5h8no4na.net>
2020-02-11 21:24:27 +0100

    Don't link to hardcoded domain, instead use server_name This prevents the local version making requests to the web version

--- global/htmlHelper.php
+++ global/htmlHelper.php
@@ -1,21 +1,26 @@
 <?php
 
 function generateHeadBoilerplate() {
-    ?><meta charset="utf-8" />
+    $tmp = explode(".", $_SERVER["SERVER_NAME"]);
+    $tmp = array_reverse($tmp);
+    $domain = $tmp[1] . "." . $tmp[0];
+    $prefix = $_SERVER["REQUEST_SCHEME"] . "://" . $domain;
+    echo <<< "HTML"
+    <meta charset="utf-8" />
     <!--Favicon stuff-->
-    <link rel="apple-touch-icon" sizes="180x180" href="https://c5h8no4na.net/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://c5h8no4na.net/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://c5h8no4na.net/favicons/favicon-16x16.png">
-    <link rel="manifest" href="https://c5h8no4na.net/favicons/site.webmanifest">
-    <link rel="mask-icon" href="https://c5h8no4na.net/favicons/safari-pinned-tab.svg" color="#00cc4a">
-    <link rel="shortcut icon" href="https://c5h8no4na.net/favicons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="{$prefix}/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{$prefix}/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{$prefix}/favicons/favicon-16x16.png">
+    <link rel="manifest" href="{$prefix}/favicons/site.webmanifest">
+    <link rel="mask-icon" href="{$prefix}/favicons/safari-pinned-tab.svg" color="#00cc4a">
+    <link rel="shortcut icon" href="{$prefix}/favicons/favicon.ico">
     <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="msapplication-config" content="https://c5h8no4na.net/favicons/browserconfig.xml">
+    <meta name="msapplication-config" content="{$prefix}/favicons/browserconfig.xml">
     <meta name="theme-color" content="#00cc4a">
-    <link rel="stylesheet" href="https://c5h8no4na.net/darkmode.css">
+    <link rel="stylesheet" href="{$prefix}/darkmode.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<?php
+HTML;
 }
 
 function redirectToFolderOfFile() {
