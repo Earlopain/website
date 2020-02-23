4f128e2ad571fe0a6dccb2b974b909d034b9835f
Earlopain <earlopain@c5h8no4na.net>
2020-02-23 22:46:46 +0100

    Added functionality to empty the queue

--- projects/e621history/queueWorker.php
+++ projects/e621history/queueWorker.php
@@ -1,9 +1,17 @@
 <?php
 $fp = fopen('/tmp/php-commit.lock', 'c');
 if (!flock($fp, LOCK_EX | LOCK_NB)) {
-    file_put_contents("test.log", "no lock");
     exit;
 }
-file_put_contents("test.log", "lock aquired");
-sleep(5);
+
+require_once "userFavHistory.php";
+require_once "queue.php";
+
+$allUsers = E621UserQueue::getFullQueue();
+
+foreach ($allUsers as $username) {
+    UserfavHistory::populateDb($username);
+    E621UserQueue::removeFromQueue($username);
+}
+
 flock($fp, LOCK_UN);
