0c493f68a076931bccab9f677db08b537cef8333
Earlopain <earlopain@c5h8no4na.net>
2020-02-27 00:27:53 +0100

    Revert using unbuffered sql, not needed anymore

--- global/sql.php
+++ global/sql.php
@@ -10,7 +10,6 @@ class SqlConnection {
             $username = Secret::get("dbuser");
             $password = Secret::get("dbpass");
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$connections[$dbname] = $conn;
         }
