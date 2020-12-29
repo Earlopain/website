d5462739303124ca35df6599362a03f6b95a86f2
Earlopain <earlopain@c5h8no4na.net>
2020-12-29 19:04:18 +0100

    Reorganize buttons

--- internal/index.php
+++ internal/index.php
@@ -10,51 +10,56 @@ generateHeadBoilerplate();?>
 </head>
 
 <body>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('apache2restart')">Apache2 Restart</button>
+    <div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('e621dl')">E621 Download</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="getFileFromServer('/media/plex/software/e621comics/pools.json')">E621 Edit Comics</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('plexrefreshcomics')">E621 Refresh Comics</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('e621replace')">E621 Replacer</button>
+        </div>
     </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('plexrestart')">Plex Restart</button>
+    <div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('deezerdl')">Deezer Download</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('musicvideo')">Youtube Musicvideo</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('shortmovie')">Short Movies</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('youtube')">Youtube Download</button>
+        </div>
     </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('plexrefreshcomics')">Plex Refresh Comics</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="getFileFromServer('/media/plex/software/e621comics/pools.json')">Plex Edit Comics</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('plextagimages')">Plex Tag Images</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('plexfixdates')">Plex Fix Dates</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('deezerdl')">Deezer Download</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="getFileFromServer('/srv/http/.config/smloadr/SMLoadrConfig.json')">Deezer Set ARL</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('e621dl')">E621 Download</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('e621replace')">E621 Replacer</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('musicvideo')">Youtube Musicvideo</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('shortmovie')">Short Movies</button>
-    </div>
-    <div class="buttoncontainer">
-        <button class="button" onclick="executeOnServer('youtube')">Youtube Download</button>
+    <div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('apache2restart')">Apache2 Restart</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('plexrestart')">Plex Restart</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('plextagimages')">Plex Tag Images</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="executeOnServer('plexfixdates')">Plex Fix Dates</button>
+        </div>
+        <div class="buttoncontainer">
+            <button class="button" onclick="getFileFromServer('/srv/http/.config/smloadr/SMLoadrConfig.json')">Deezer Set ARL</button>
+        </div>
     </div>
     <p></p>
     <div class="buttoncontainer">
         <textarea id="commandout" class="output button"></textarea>
         <button id="submitfile" class="button" onclick="putFileOnServer()" style="display: none">Submit File</button>
     </div>
-
 </body>
 
 </html>
