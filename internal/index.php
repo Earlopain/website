bd1dd2e1ae77e7b8ea416ef4ac1289b1839f39d2
Earlopain <earlopain@c5h8no4na.net>
2020-12-30 21:56:57 +0100

    Split button/textarea height 50/50

--- internal/index.php
+++ internal/index.php
@@ -10,28 +10,26 @@ generateHeadBoilerplate();?>
 </head>
 
 <body>
-    <div>
-        <div class="buttoncontainer">
+    <div id="container">
+        <div id=buttoncontainer>
             <button data-type="command" data-extra="e621dl">E621 Download</button>
             <button data-type="getfile" data-extra="e621pools">E621 Edit Comics</button>
             <button data-type="command" data-extra="plexrefreshcomics">E621 Refresh Comics</button>
             <button data-type="command" data-extra="e621replace">E621 Replacer</button>
             <button data-type="command" data-extra="sofurryepub">Sofurry EPUB</button>
-        </div>
-        <div class="buttoncontainer">
+            <div></div>
             <button data-type="command" data-extra="deezerdl">Deezer Download</button>
             <button data-type="command" data-extra="musicvideo">Youtube Musicvideo</button>
             <button data-type="command" data-extra="shortmovie">Short Movies</button>
             <button data-type="command" data-extra="youtube">Youtube Download</button>
-        </div>
-        <div class="buttoncontainer">
+            <div></div>
             <button data-type="command" data-extra="apache2restart">Apache2 Restart</button>
             <button data-type="command" data-extra="plexrestart">Plex Restart</button>
             <button data-type="command" data-extra="plextagimages">Plex Tag Images</button>
             <button data-type="command" data-extra="plexfixdates">Plex Fix Dates</button>
             <button data-type="getfile" data-extra="smloadrconfig">Deezer Set ARL</button>
         </div>
-        <div class="outputcontainer">
+        <div id="outputcontainer">
             <textarea id="textarea"></textarea>
             <button data-type="savefile" id="submitfile" style="display: none">Submit File</button>
         </div>
